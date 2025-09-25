f2bdceeb303510093067a4c2476a78372610371f
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-09-25 12:03:28 +0100

    Add basic skeleton

--- src/i19serial_ui/gui/serial_gui_eh2.py
+++ src/i19serial_ui/gui/serial_gui_eh2.py
@@ -1,6 +1,38 @@
 from PyQt6 import QtWidgets
 
+WINDOW_SIZE = (700, 1200)
+
 
 class SerialGuiEH2(QtWidgets.QMainWindow):
     def __init__(self) -> None:
+        pass
+
+    def init_gui(self):
+        # self._create_toolbar()
+        pass
+
+    def _create_toolbar(self):
+        pass
+
+    def _create_title_box(self):
+        pass
+
+    def _create_top_group(self):
+        # move arrows, phi step, focuse, backlight etc
+        pass
+
+    def _create_coordinate_system_group(self):
+        pass
+
+    def _create_collection_inputs_group(self):
+        pass
+
+    def _create_collection_buttons_group(self):
+        pass
+
+    def _create_bottom_group(self):
+        # log panel view
+        pass
+
+    def create_main_layout(self):
         pass
