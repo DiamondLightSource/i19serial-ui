9d12f70a6702853af6e83d774643f5f5ae4c60ee
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-09-25 12:14:24 +0100

    Set up the log GUI handler

---
+++ src/i19serial_ui/log.py
@@ -0,0 +1,36 @@
+# NOTE For actual logs, we'll use the dodal/blueapi logger
+# But we do need something that prints to the UI
+
+import logging
+
+from PyQt6.QtCore import QObject, pyqtSignal
+
+LOGGER = logging.getLogger("i19serial-ui")
+LOGGER.setLevel(logging.DEBUG)
+
+
+class GuiWindowLogHandler(logging.Handler, QObject):
+    signalLog = pyqtSignal(str)  # noqa: N815
+
+    def __init__(self):
+        super().__init__()
+        QObject.__init__(self)
+        gui_formatter = logging.Formatter("%(message)s")
+
+        self.setFormatter(gui_formatter)
+        self.setLevel(logging.INFO)
+
+    def emit(self, record: logging.LogRecord):
+        msg = self.format(record)
+        self.signalLog.emit(msg)
+
+
+def log_to_gui(logger: logging.Logger, output_str: str, level: str = "INFO"):
+    logger.log(getattr(logging, level.upper()), f"{output_str}")
+
+
+def tidy_up_logging(loggers: list[logging.Logger]):
+    for logger in loggers:
+        for handler in logger.handlers:
+            handler.close()
+        logger.handlers.clear()
