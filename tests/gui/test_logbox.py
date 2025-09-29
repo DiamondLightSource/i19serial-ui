8a734d2048a1b91fb8f2727aacec5ab0531db020
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-09-29 15:08:42 +0100

    Add a couple of UI tests for logbox

---
+++ tests/gui/test_logbox.py
@@ -0,0 +1,32 @@
+import logging
+
+import pytest
+from PyQt6 import QtWidgets
+
+from i19serial_ui.gui.log_box import GREETING, LogBox
+from i19serial_ui.log import GuiWindowLogHandler
+
+
+@pytest.fixture
+def mock_gui_handler():
+    return GuiWindowLogHandler()
+
+
+@pytest.fixture
+def fake_log_box(mock_gui_handler, qtbot):
+    test_box = LogBox(QtWidgets.QWidget(), mock_gui_handler)
+    qtbot.addWidget(test_box)
+    return test_box
+
+
+def test_start_up_greeting_in_log_box(fake_log_box):
+    assert fake_log_box.log_output_box.toPlainText() == GREETING
+
+
+def test_log_box_updated_with_log_message(fake_log_box, mock_gui_handler):
+    log_record = logging.LogRecord(
+        "Info", logging.INFO, "", 0, "Hello test", None, None
+    )
+    mock_gui_handler.emit(log_record)
+    full_log = fake_log_box.log_output_box.toPlainText()
+    assert full_log.split("\n")[-1] == "Hello test"
