1d38e9cfa925b88b4c281e3874fd1fbf09d73364
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-10-06 15:41:21 +0000

    Basic tests for input panel

--- tests/gui/test_logbox.py
+++ tests/gui/test_logbox.py
@@ -13,20 +13,20 @@ def mock_gui_handler():
 
 
 @pytest.fixture
-def fake_log_box(mock_gui_handler, qtbot):
+def mock_log_box(mock_gui_handler, qtbot):
     test_box = LogBox(QtWidgets.QWidget(), mock_gui_handler)
     qtbot.addWidget(test_box)
     return test_box
 
 
-def test_start_up_greeting_in_log_box(fake_log_box):
-    assert fake_log_box.log_output_box.toPlainText() == GREETING
+def test_start_up_greeting_in_log_box(mock_log_box):
+    assert mock_log_box.log_output_box.toPlainText() == GREETING
 
 
-def test_log_box_updated_with_log_message(fake_log_box, mock_gui_handler):
+def test_log_box_updated_with_log_message(mock_log_box, mock_gui_handler):
     log_record = logging.LogRecord(
         "Info", logging.INFO, "", 0, "Hello test", None, None
     )
     mock_gui_handler.emit(log_record)
-    full_log = fake_log_box.log_output_box.toPlainText()
+    full_log = mock_log_box.log_output_box.toPlainText()
     assert full_log.split("\n")[-1] == "Hello test"
