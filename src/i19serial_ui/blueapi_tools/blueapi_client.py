94e99ddffe08b5646ef4a8ef9db0aca44a9853cf
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-10-28 10:21:36 +0000

    Add a minimal client

--- src/i19serial_ui/blueapi_tools/blueapi_client.py
+++ src/i19serial_ui/blueapi_tools/blueapi_client.py
@@ -1,5 +1,19 @@
+from pathlib import Path
+
 from blueapi.client.client import BlueapiClient
+from blueapi.config import ApplicationConfig, ConfigLoader
 
 
-class BAClient(BlueapiClient):
-    pass
+class SerialBlueapiClient:
+    def __init__(self, config_file: Path, session: str = ""):
+        self.instrument_session = session
+        self.client = self._get_blueapi_client(config_file)
+
+    def _get_blueapi_client(self, config_file) -> BlueapiClient:
+        config_loader = ConfigLoader(ApplicationConfig)
+        config_loader.use_values_from_yaml(config_file)
+        config = config_loader.load()
+        return BlueapiClient.from_config(config)
+
+    def update_session(self, new_session: str):
+        self.instrument_session = new_session
