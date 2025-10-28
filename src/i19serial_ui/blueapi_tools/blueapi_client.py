96a829765ffde84b48e7479d3c286e39a9904f3c
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-10-28 16:00:19 +0000

    A first working basic version of the client

--- src/i19serial_ui/blueapi_tools/blueapi_client.py
+++ src/i19serial_ui/blueapi_tools/blueapi_client.py
@@ -1,22 +1,53 @@
 from pathlib import Path
+from typing import Any
 
 from blueapi.client.client import BlueapiClient
+from blueapi.client.rest import BlueskyRemoteControlError
 from blueapi.config import ApplicationConfig, ConfigLoader
+from blueapi.service.model import TaskRequest
 
 from i19serial_ui.log import LOGGER, log_to_gui
 
 
+class WrongConfigFileFormatError(Exception):
+    pass
+
+
 class SerialBlueapiClient:
     def __init__(self, config_file: Path, session: str = ""):
+        # TODO Needs a callback for logging reasons, probably
         self.instrument_session = session
         self.client = self._get_blueapi_client(config_file)
 
-    def _get_blueapi_client(self, config_file) -> BlueapiClient:
+    def _load_config_from_file(self, config_file) -> ApplicationConfig:
         config_loader = ConfigLoader(ApplicationConfig)
+        if config_file.suffix != ".yaml":
+            raise WrongConfigFileFormatError(
+                """The config file passed for Blueapi is not a yaml file.
+                Impossible to start up the client."""
+            )
         config_loader.use_values_from_yaml(config_file)
-        config = config_loader.load()
+        return config_loader.load()
+
+    def _get_blueapi_client(self, config_file) -> BlueapiClient:
+        config = self._load_config_from_file(config_file)
         return BlueapiClient.from_config(config)
 
     def update_session(self, new_session: str):
         self.instrument_session = new_session
         log_to_gui(LOGGER, f"Session updated: {self.instrument_session}")
+
+    def abort_task(self):
+        try:
+            self.client.abort()
+        except BlueskyRemoteControlError:
+            # TO avoid GUI dying on double clicks
+            log_to_gui(LOGGER, "Abort: Nothing seems to be running.")
+
+    def run_plan(self, plan_name: str, plan_params: dict[str, Any]):
+        task = TaskRequest(
+            name=plan_name,
+            params=plan_params,
+            instrument_session=self.instrument_session,
+        )
+        self.client.create_and_start_task(task)
