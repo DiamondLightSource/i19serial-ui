65f07235a6845a53f89f3fc7bc2a351f4decdc39
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-10-29 10:23:01 +0000

    Add a test for select_visit

--- tests/gui/test_eh2_ui.py
+++ tests/gui/test_eh2_ui.py
@@ -34,3 +34,14 @@ def test_general_layout(mock_eh2_gui):
     assert isinstance(mock_eh2_gui.general_layout, QtWidgets.QGridLayout)
     title = mock_eh2_gui.general_layout.children()[0]
     assert isinstance(title, QtWidgets.QHBoxLayout)
+
+
+def test_select_visit(mock_eh2_gui):
+    with patch(
+        "i19serial_ui.gui.serial_gui_eh2.QtWidgets.QFileDialog.getExistingDirectory"
+    ) as patch_dir:
+        patch_dir.return_value = "/path/to/data/cm12345-1"
+        mock_eh2_gui.select_visit_action.trigger()
+
+        assert mock_eh2_gui.inputs.visit_path.text() == "/path/to/data/cm12345-1"
+        mock_eh2_gui.client.update_session.assert_called_once_with("cm12345-1")
