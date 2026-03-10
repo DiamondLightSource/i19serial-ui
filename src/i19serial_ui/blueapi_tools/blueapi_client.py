4e4350996155a149eb3820f345b3801251c6dae6
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-03-10 13:46:58 +0000

    Start tidying up

--- src/i19serial_ui/blueapi_tools/blueapi_client.py
+++ src/i19serial_ui/blueapi_tools/blueapi_client.py
@@ -34,6 +34,23 @@ class SerialBlueapiClient:
         config = self._load_config_from_file(config_file)
         return BlueapiClient.from_config(config)
 
+    def _create_task(self, plan_name: str, plan_params: dict[str, Any]) -> TaskRequest:
+        return TaskRequest(
+            name=plan_name,
+            params=plan_params,
+            instrument_session=self.instrument_session,
+        )
+
+    def _check_instrument_session(self) -> bool:
+        if not self.instrument_session:
+            log_to_gui(
+                LOGGER,
+                "Instrument session hasn't been set, please select visit before continuing.",  # noqa: E501
+                level="ERROR",
+            )
+            return False
+        return True
+
     def update_session(self, new_session: str):
         self.instrument_session = new_session
         log_to_gui(LOGGER, f"Session updated: {self.instrument_session}")
@@ -46,27 +63,35 @@ class SerialBlueapiClient:
             log_to_gui(LOGGER, "Abort: Nothing seems to be running.")
 
     def run_plan(self, plan_name: str, plan_params: dict[str, Any]):
-        if not self.instrument_session:
-            log_to_gui(
-                LOGGER,
-                "Instrument session hasn't been set, please select visit before continuing.",  # noqa: E501
-                level="ERROR",
-            )
-            return
-        task = TaskRequest(
-            name=plan_name,
-            params=plan_params,
-            instrument_session=self.instrument_session,
-        )
-        # self.client.create_and_start_task(task)
-        try:
-            result = self.client.run_task(task)
-        except ServiceUnavailableError:
-            print("error creating task")
-        except Exception as e:
-            print(f"Some other issue {e}")
+        if self._check_instrument_session():
+            task = self._create_task(plan_name, plan_params)
+            try:
+                self.client.create_and_start_task(task)
+            except Exception as e:
+                log_to_gui(
+                    LOGGER,
+                    f"""Something went wrong running the {plan_name} plan,
+                    check out the logs for more information""",
+                    level="ERROR",
+                )
+                LOGGER.exception(e)
+        log_to_gui(LOGGER, "Run plan done", level="DEBUG")
 
-        if result.task_failed:
-            print("somethign went wrong when running the plan, look at blueapi logs")
-        else:
-            print(result.result)
+    def run_plan_and_get_result(self, plan_name: str, plan_params: dict[str, Any]):
+        if self._check_instrument_session():
+            task = self._create_task(plan_name, plan_params)
+            try:
+                result = self.client.run_task(task)
+            except ServiceUnavailableError as e:
+                print("error creating task")
+                LOGGER.exception(e)
+            except Exception as e:
+                print(f"Some other issue {e}")
+                LOGGER.exception(e)
+            if result.task_failed:
+                print(
+                    "somethign went wrong when running the plan, look at blueapi logs"
+                )
+            else:
+                print(result.result)
+        log_to_gui(LOGGER, "Run plan with result done", level="DEBUG")
