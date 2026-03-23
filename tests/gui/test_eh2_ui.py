0b6549178b27913bfe90a3dfdf92167d6800f94c
Matt-Carre <matthew.carre@diamond.ac.uk>
2026-03-23 09:18:29 +0000

    Merge run buttons and run serial plan (#41)
    * Added dropdown, need tests
    
    * Added unit testing of GUI and labels to drop-down box
    
    * Added unit testing of GUI and labels to drop-down box, altered one unit test
    
    * Added dropdown, need tests
    
    * Added unit testing of GUI and labels to drop-down box
    
    * Added unit testing of GUI and labels to drop-down box, altered one unit test
    
    * Edited post pull.
    
    * changed layout of top box, saved choice as variable, moved aperture size into general_utils file
    
    * Altering remaining code according to comments
    
    * Altering remaining code according to comments - remembering to alter variable names properly
    
    * changing gui and test to remove zebra, run only main_entry_plan
    
    * Added dropdown, need tests
    
    * changed layout of top box, saved choice as variable, moved aperture size into general_utils file
    
    * Collected detector_z from ui
    
    * rewrote test
    
    * updated name of plan
    
    * updated name of plan in testing
    
    * renames button based on comments
    
    * altered remaining comments

--- tests/gui/test_eh2_ui.py
+++ tests/gui/test_eh2_ui.py
@@ -66,36 +66,25 @@ def test_abort_button(mock_eh2_gui):
     mock_eh2_gui.client.abort_task.assert_called_once()
 
 
-def test_run_zebra(mock_eh2_gui):
-    mock_rotation_start = 0.0
-    mock_num_images = 50.0
-    mock_rotation_increment = 0.2
-    mock_rotation_end = mock_rotation_start + mock_num_images + mock_rotation_increment
-    mock_time_image = 0.2
-
-    mock_params = {
-        "phi_start": mock_rotation_start,
-        "phi_end": mock_rotation_end,
-        "phi_steps": mock_num_images,
-        "exposure_time": mock_time_image,
-        "gate_width": mock_rotation_end - mock_rotation_start + 0.1,
-        "pulse_width": mock_rotation_increment,
-    }
-    mock_eh2_gui.test_btn1.click()
-    mock_eh2_gui.client.run_plan.assert_called_once_with("run_zebra_test", mock_params)
-
-
 def test_run_panda(mock_eh2_gui):
+    mock_detector_z = 117.53
+    mock_eh2_aperture = "20um"  # ApertureOptions.UM_20
+    mock_detector_two_theta = 0.0
     mock_rotation_start = 0.0
     mock_num_images = 50.0
     mock_rotation_increment = 0.2
     mock_rotation_end = mock_rotation_start + mock_num_images + mock_rotation_increment
     mock_time_image = 0.2
     mock_params = {
+        "detector_z": mock_detector_z,
+        "detector_two_theta": mock_detector_two_theta,
         "phi_start": mock_rotation_start,
         "phi_end": mock_rotation_end,
         "phi_steps": mock_num_images,
         "exposure_time": mock_time_image,
+        "eh2_aperture": mock_eh2_aperture,
     }
-    mock_eh2_gui.test_btn2.click()
-    mock_eh2_gui.client.run_plan.assert_called_once_with("run_panda_test", mock_params)
+    mock_eh2_gui.run_btn.click()
+    mock_eh2_gui.client.run_plan.assert_called_once_with(
+        "run_serial_from_panda", mock_params
+    )
