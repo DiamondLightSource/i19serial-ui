bcd9479d53b6102725a8dffc5707ac147a79cc5f
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-04-15 19:02:56 +0100

    Test layout of the cs widget

--- tests/gui/test_cs_panel.py
+++ tests/gui/test_cs_panel.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 
 import pytest
 from blueapi.config import ApplicationConfig
+from PyQt6 import QtWidgets
 
 from i19serial_ui.blueapi_tools.blueapi_client import SerialBlueapiClient
 from i19serial_ui.gui.widgets.cs_panel import CoordinateSystemPanel
@@ -24,3 +25,14 @@ def mock_cs_panel(mock_client, qtbot):
     test_panel = CoordinateSystemPanel(mock_client, GridType.POLYMER, (3, 3))
     qtbot.addWidget(test_panel)
     return test_panel
+
+
+def test_cs_panel_layout(mock_cs_panel):
+    assert mock_cs_panel.cs_layout is not None
+    assert isinstance(mock_cs_panel.cs_layout, QtWidgets.QVBoxLayout)
+    assert mock_cs_panel.cs_layout.count() == 2
+
+    (pos_layout, btn_layout) = mock_cs_panel.cs_layout.children()
+
+    assert isinstance(pos_layout, QtWidgets.QGridLayout)
+    assert isinstance(btn_layout, QtWidgets.QHBoxLayout)
