b7891eb28a5a126006f52f7b3e5f5835acfc0f16
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-04-20 14:51:19 +0100

    Add translation calculation

--- src/i19serial_ui/coordinate_system/utils.py
+++ src/i19serial_ui/coordinate_system/utils.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from i19serial_ui.parameters.coordinates import (
     COORD_FILE_PATH,
+    Coord3D,
     Coordinates,
     FiducialPosition,
 )
@@ -26,6 +27,14 @@ def save_coordinates_to_json(filename: Path | str, coordinates: Coordinates):
     full_filename = COORD_FILE_PATH / filename
     with open(full_filename, "w") as fh:
         json.dump(coordinates.model_dump(), fh, indent=4)
+
+
+def _find_translated_coordinates(
+    fiducial_coords: tuple[float, float, float],
+    translation_vector: tuple[float, float, float],
+) -> Coord3D:
+    res = [(i + j) for i, j in zip(fiducial_coords, translation_vector, strict=True)]
+    return Coord3D(*res)
 
 
 def work_out_fiducial_positions_from_ui_input(
