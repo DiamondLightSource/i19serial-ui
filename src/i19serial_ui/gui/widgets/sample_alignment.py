1255e34a28f0838f6e7b31a1828382a2c0dc0117
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-07-23 16:52:43 +0000

    Add images to assets and start bare bones of UI

---
+++ src/i19serial_ui/gui/widgets/sample_alignment.py
@@ -0,0 +1,25 @@
+from PyQt6 import QtWidgets
+
+from i19serial_ui.blueapi_tools.blueapi_client import SerialBlueapiClient
+
+# from i19serial_ui.log import LOGGER
+
+# Wrapper around bps.mv from dodal.plan_stubs
+MOVE_PLAN = "move"
+
+
+class SampleAlignment(QtWidgets.QWidget):
+    def __init__(
+        self,
+        blueapi_client: SerialBlueapiClient,
+        parent: QtWidgets.QWidget | None = None,
+    ):
+        super().__init__(parent)
+        self.client = blueapi_client
+        self.init_arrow_buttons()
+
+    def init_arrow_buttons(self):
+        pass
+
+    def create_layout(self):
+        pass
