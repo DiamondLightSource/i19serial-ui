df2b06db74687ef25d9bb5292bb87145255f028e
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-08-05 11:44:17 +0100

    Remove comment out for test

--- src/i19serial_ui/gui/ui_utils.py
+++ src/i19serial_ui/gui/ui_utils.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from enum import StrEnum
 from importlib.resources import files
@@ -63,11 +64,11 @@ def parse_dataset_input(visit: str, dataset: str | None, prefix: str | None) -> 
             level="ERROR",
         )
         return False
-    # if os.path.isdir(f"{visit}/{dataset}"):
-    #     log_to_gui(
-    #         LOGGER,
-    #         "Data collection folder already exsists, please choose unique name",
-    #         level="ERROR",
-    #     )
-    #     return False
+    if os.path.isdir(f"{visit}/{dataset}"):
+        log_to_gui(
+            LOGGER,
+            "Data collection folder already exsists, please choose unique name",
+            level="ERROR",
+        )
+        return False
     return True
